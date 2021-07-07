7af2550858ad2fc9574af73a1a555fdffe96c8cb
DavidLiu <david.yx.liu@oracle.com>
2021-07-08 00:33:12 +0800

    [WIP] Error: Internal Server Error

--- vault/index.js
+++ vault/index.js
@@ -1,4 +1,6 @@
-const {KmsVaultClient, KmsManagementClient, models: {VaultSummary, KeySummary}} = require('oci-keymanagement')
+const {models} = require('oci-keymanagement')
+
+const {KmsVaultClient, KmsManagementClient, KmsCryptoClient} = require('oci-keymanagement/lib/client')
 
 /**
  * https://github.com/oracle/oci-typescript-sdk/blob/master/examples/javascript/keymanagement.js
@@ -16,18 +18,73 @@ class Vault {
     /**
      *
      * @param {string} compartmentId
-     * @return {Promise<VaultSummary[]>}
+     * @return {Promise<models.VaultSummary[]>}
      */
     async list(compartmentId) {
         const request = {compartmentId}
         const {items} = await this.vault.listVaults(request)
         return items.filter(({lifecycleState}) => lifecycleState !== KeySummary.LifecycleState.Deleted)
     }
+
+    /**
+     *
+     * @param {string} vaultId - OCID of vault
+     * @return {Promise<models.Vault>}
+     */
+    async get(vaultId) {
+        const {vault} = await this.vault.getVault({vaultId})
+        return vault
+    }
 }
 
 class Key {
-    constructor() {
-        // TODO WIP
+    /**
+     *
+     * @param {_Connector} connector
+     * @param {models.VaultSummary|models.Vault} vault
+     */
+    constructor(connector, vault) {
+        const {provider} = connector
+        const {cryptoEndpoint, managementEndpoint} = vault
+        const kms = new KmsManagementClient({authenticationDetailsProvider: provider})
+        kms.endpoint = managementEndpoint
+        const cryptoOperator = new KmsCryptoClient({authenticationDetailsProvider: provider})
+        cryptoOperator.endpoint = cryptoEndpoint
+
+        Object.assign(this, {kms, cryptoOperator})
+    }
+
+    async list(compartmentId) {
+        const {items} = await this.kms.listKeys({compartmentId})
+        return items
+    }
+
+    /**
+     * @param keyId OCID of key
+     * @return {Promise<models.Key>}
+     */
+    async get(keyId) {
+        const {key} = await this.kms.getKey({keyId})
+        return key
+    }
+
+    async encrypt(keyId, plaintext, keyVersionId) {
+        const encryptDataDetails = {
+            keyId,
+            plaintext,
+            keyVersionId
+        }
+
+        return await this.cryptoOperator.encrypt({encryptDataDetails})
+    }
+
+    async sign(keyId, message) {
+        const signDataDetails = {
+            message,
+            keyId,
+        }
+        // FIXME: Error: Internal Server Error
+        return await this.cryptoOperator.sign({signDataDetails})
     }
 }
 
--- vault/test/index.js
+++ vault/test/index.js
@@ -1,12 +1,43 @@
-const {Vault} = require('../index')
+const {Vault, Key} = require('../index')
 const {FileAuthentication} = require('../../index')
+const auth = new FileAuthentication()
+const compartmentId = 'ocid1.compartment.oc1..aaaaaaaaw2hdbvkul6ocyl6lrowdiu3y44sop4owoya5nrmhlsx7d3gbyrea'
+const assert = require('assert')
 describe('vault', () => {
-    it('connect', async () => {
-        const auth = new FileAuthentication()
-        await auth.connect()
-        const vault = new Vault(auth)
-        const compartmentId = 'ocid1.compartment.oc1..aaaaaaaaw2hdbvkul6ocyl6lrowdiu3y44sop4owoya5nrmhlsx7d3gbyrea'
+    const vault = new Vault(auth)
+    before(async () => {
+        const health = await auth.connect()
+        assert.ok(health)
+    })
+    it('list', async () => {
+
+
         const result = await vault.list(compartmentId)
         console.debug(result)
+    })
+})
+describe('key', () => {
+
+    const vaultId = 'ocid1.vault.oc1.ap-seoul-1.bzp726oeaagiu.abuwgljr35dzjn3okjopehwmvhqtb6wx3jhw4uu3no56qita35rx4wy4zheq'
+    const keyID = 'ocid1.key.oc1.ap-seoul-1.bzp726oeaagiu.abuwgljrua4t3ffaq6bsouv3y5r34udkdjiizdicaj27oitka5rp3u764caa'
+    const vault = new Vault(auth)
+    let oneVault
+    before(async () => {
+        const health = await auth.connect()
+        assert.ok(health)
+        oneVault = await vault.get(vaultId)
+    })
+    it('list', async () => {
+
+        const key = new Key(auth, oneVault)
+
+        const keyList = await key.list(compartmentId)
+        console.debug(keyList)
+    })
+    it('encrypt', async () => {
+
+        const key = new Key(auth, oneVault)
+
+        await key.sign(keyID, 'abc')
     })
 })
