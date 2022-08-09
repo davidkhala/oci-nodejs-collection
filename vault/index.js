b5bf044abc8f28a27e08dbb6a24d5f591e60bb89
davidliu <david-khala@hotmail.com>
2022-08-09 23:01:25 +0800

    OCI KMS: digest message cannot be longer than 32 bytes

--- vault/index.js
+++ vault/index.js
@@ -1,161 +1,169 @@
-import {KmsVaultClient, KmsManagementClient, KmsCryptoClient, models} from 'oci-keymanagement'
-import {DefaultSigningAlgorithm} from './convention.js'
-import assert from "assert";
+import {KmsVaultClient, KmsManagementClient, KmsCryptoClient, models} from 'oci-keymanagement';
+import {DefaultSigningAlgorithm} from './convention.js';
+import assert from 'assert';
 
-const {SignDataDetails: {SigningAlgorithm}, KeySummary, VaultSummary} = models
+const {SignDataDetails: {SigningAlgorithm, MessageType}, KeySummary, VaultSummary} = models;
 
 /**
  * https://github.com/oracle/oci-typescript-sdk/blob/master/examples/javascript/keymanagement.js
  */
 class VaultManager {
-    /**
-     *
-     * @param {_Connector} connector
-     */
-    constructor(connector) {
-        const {provider} = connector
-        this.vault = new KmsVaultClient({authenticationDetailsProvider: provider})
-    }
+	/**
+	 *
+	 * @param {_Connector} connector
+	 */
+	constructor(connector) {
+		const {provider} = connector;
+		this.vault = new KmsVaultClient({authenticationDetailsProvider: provider});
+	}
 
-    /**
-     *
-     * @param {string} compartmentId
-     * @return {Promise<VaultSummary[]>}
-     */
-    async list(compartmentId) {
-        const request = {compartmentId}
-        const {items} = await this.vault.listVaults(request)
-        return items.filter(({lifecycleState}) => lifecycleState !== KeySummary.LifecycleState.Deleted)
-    }
+	/**
+	 *
+	 * @param {string} compartmentId
+	 * @return {Promise<VaultSummary[]>}
+	 */
+	async list(compartmentId) {
+		const request = {compartmentId};
+		const {items} = await this.vault.listVaults(request);
+		return items.filter(({lifecycleState}) => lifecycleState !== KeySummary.LifecycleState.Deleted);
+	}
 
-    /**
-     *
-     * @param {string} vaultId - OCID of vault
-     * @return {Promise<Vault>}
-     */
-    async get(vaultId) {
-        const {vault} = await this.vault.getVault({vaultId})
-        return vault
-    }
+	/**
+	 *
+	 * @param {string} vaultId - OCID of vault
+	 * @return {Promise<Vault>}
+	 */
+	async get(vaultId) {
+		const {vault} = await this.vault.getVault({vaultId});
+		return vault;
+	}
 }
 
 class KeyOperator {
-    /**
-     *
-     * @param {_Connector} connector
-     * @param {model.VaultSummary|model.Vault} vault
-     * @param {string} [keyId]
-     */
-    constructor(connector, vault, keyId) {
-        const {provider} = connector
-        const {cryptoEndpoint, managementEndpoint, compartmentId} = vault
-        const kms = new KmsManagementClient({authenticationDetailsProvider: provider})
-        kms.endpoint = managementEndpoint
-        const cryptoOperator = new KmsCryptoClient({authenticationDetailsProvider: provider})
-        cryptoOperator.endpoint = cryptoEndpoint
+	/**
+	 *
+	 * @param {_Connector} connector
+	 * @param {model.VaultSummary|model.Vault} vault
+	 * @param {string} [keyId]
+	 */
+	constructor(connector, vault, keyId) {
+		const {provider} = connector;
+		const {cryptoEndpoint, managementEndpoint, compartmentId} = vault;
+		const kms = new KmsManagementClient({authenticationDetailsProvider: provider});
+		kms.endpoint = managementEndpoint;
+		const cryptoOperator = new KmsCryptoClient({authenticationDetailsProvider: provider});
+		cryptoOperator.endpoint = cryptoEndpoint;
 
-        Object.assign(this, {kms, cryptoOperator, keyId, compartmentId})
-    }
+		Object.assign(this, {kms, cryptoOperator, keyId, compartmentId});
+	}
 
-    /**
-     *
-     * @param [compartmentId]
-     * @returns {Promise<*>}
-     */
-    async list(compartmentId = this.compartmentId) {
-        const {items} = await this.kms.listKeys({compartmentId})
-        return items
-    }
+	/**
+	 *
+	 * @param [compartmentId]
+	 * @returns {Promise<*>}
+	 */
+	async list(compartmentId = this.compartmentId) {
+		const {items} = await this.kms.listKeys({compartmentId});
+		return items;
+	}
 
-    /**
-     * @return {Promise<model.Key>}
-     */
-    async get() {
-        const {keyId} = this
-        const {key} = await this.kms.getKey({keyId})
-        return key
-    }
+	/**
+	 * @return {Promise<model.Key>}
+	 */
+	async get() {
+		const {keyId} = this;
+		const {key} = await this.kms.getKey({keyId});
+		return key;
+	}
 
-    async publicKey() {
-        const {keyId} = this
-        const {currentKeyVersion, keyShape: {algorithm}} = await this.get()
-        assert.ok(algorithm === 'ECDSA' || algorithm === 'RSA')
+	async publicKey() {
+		const {keyId} = this;
+		const {currentKeyVersion, keyShape: {algorithm}} = await this.get();
+		assert.ok(algorithm === 'ECDSA' || algorithm === 'RSA');
 
-        const {keyVersion} = await this.kms.getKeyVersion({
-            keyId,
-            keyVersionId: currentKeyVersion
-        })
-        return keyVersion.publicKey
-    }
+		const {keyVersion} = await this.kms.getKeyVersion({
+			keyId,
+			keyVersionId: currentKeyVersion
+		});
+		return keyVersion.publicKey;
+	}
 
-    async encrypt(plaintext, keyVersionId) {
-        const {keyId} = this
-        const encryptDataDetails = {
-            keyId,
-            plaintext,
-            keyVersionId
-        }
+	async encrypt(plaintext, keyVersionId) {
+		const {keyId} = this;
+		const encryptDataDetails = {
+			keyId,
+			plaintext,
+			keyVersionId
+		};
 
-        return await this.cryptoOperator.encrypt({encryptDataDetails})
-    }
+		return await this.cryptoOperator.encrypt({encryptDataDetails});
+	}
 
-    /**
-     *
-     * @param message
-     * @param {SigningAlgorithm} [signingAlgorithm]
-     * @param [keyVersionId]
-     * @return {Promise<string>} base64 format signature
-     */
-    async sign(message, signingAlgorithm, keyVersionId) {
-        const {keyId} = this
-        if (!signingAlgorithm) {
+	/**
+	 *
+	 * @param message
+	 * @param {SigningAlgorithm} [signingAlgorithm]
+	 * @param [keyVersionId]
+	 * @param {boolean} [useDigest]
+	 * @return {Promise<string>} base64 format signature
+	 */
+	async sign(message, signingAlgorithm, keyVersionId, useDigest) {
+		const {keyId} = this;
+		if (!signingAlgorithm) {
 
-            const {keyShape: {algorithm}, currentKeyVersion} = await this.get()
-            signingAlgorithm = DefaultSigningAlgorithm[algorithm]
-            if (!keyVersionId) {
-                keyVersionId = currentKeyVersion
-            }
-        }
-        const signDataDetails = {
-            message: Buffer.from(message).toString('base64'),
-            keyId,
-            keyVersionId,
-            signingAlgorithm
-        }
-        const {signedData: {signature}} = await this.cryptoOperator.sign({signDataDetails})
-        return signature
-    }
+			const {keyShape: {algorithm}, currentKeyVersion} = await this.get();
+			signingAlgorithm = DefaultSigningAlgorithm[algorithm];
+			if (!keyVersionId) {
+				keyVersionId = currentKeyVersion;
+			}
+		}
+		const signDataDetails = {
+			message: Buffer.from(message).toString('base64'),
+			keyId,
+			keyVersionId,
+			signingAlgorithm
+		};
+		if (useDigest) {
+			signDataDetails.messageType = MessageType.Digest;
+		}
+		const {signedData: {signature}} = await this.cryptoOperator.sign({signDataDetails});
+		return signature;
+	}
 
-    /**
-     *
-     * @param signature The base64-encoded binary data object denoting the cryptographic signature generated for the message.
-     * @param message
-     * @param [signingAlgorithm]
-     * @param [keyVersionId]
-     * @return {Promise<boolean>}
-     */
-    async verify(signature, message, signingAlgorithm, keyVersionId) {
-        const {keyId} = this
-        if (!signingAlgorithm) {
-            const {currentKeyVersion, keyShape: {algorithm}} = await this.get()
-            signingAlgorithm = DefaultSigningAlgorithm[algorithm]
-            if (!keyVersionId) {
-                keyVersionId = currentKeyVersion
-            }
-        }
+	/**
+	 *
+	 * @param signature The base64-encoded binary data object denoting the cryptographic signature generated for the message.
+	 * @param message
+	 * @param [signingAlgorithm]
+	 * @param [keyVersionId]
+	 * @param {boolean} [wasDigest]
+	 * @return {Promise<boolean>}
+	 */
+	async verify(signature, message, signingAlgorithm, keyVersionId, wasDigest) {
+		const {keyId} = this;
+		if (!signingAlgorithm) {
+			const {currentKeyVersion, keyShape: {algorithm}} = await this.get();
+			signingAlgorithm = DefaultSigningAlgorithm[algorithm];
+			if (!keyVersionId) {
+				keyVersionId = currentKeyVersion;
+			}
+		}
 
-        const verifyDataDetails = {
-            keyId,
-            keyVersionId,
-            signature,
-            message: Buffer.from(message).toString('base64'),
-            signingAlgorithm
-        }
-        const {verifiedData} = await this.cryptoOperator.verify({verifyDataDetails})
-        return verifiedData.isSignatureValid
-    }
+		const verifyDataDetails = {
+			keyId,
+			keyVersionId,
+			signature,
+			message: Buffer.from(message).toString('base64'),
+			signingAlgorithm
+		};
+		if (wasDigest) {
+			verifyDataDetails.messageType = MessageType.Digest;
+		}
+		const {verifiedData} = await this.cryptoOperator.verify({verifyDataDetails});
+		return verifiedData.isSignatureValid;
+	}
 }
 
 
-export {VaultManager as Vault}
-export {KeyOperator as Key}
+export {VaultManager as Vault};
+export {KeyOperator as Key};
--- vault/test/index.js
+++ vault/test/index.js
@@ -1,89 +1,103 @@
-import assert from 'assert'
-import {Vault, Key} from '../index.js'
-import {SimpleAuthentication} from '../../common/index.js'
-import {execSync} from '@davidkhala/light/devOps.js'
+import assert from 'assert';
+import {Vault, Key} from '../index.js';
+import {SimpleAuthentication} from '../../common/index.js';
+import {execSync} from '@davidkhala/light/devOps.js';
+import crypto from 'crypto';
+import {SignDataDetails} from 'oci-keymanagement/lib/model/sign-data-details.js';
 
-const auth = new SimpleAuthentication(process.env)
-const compartmentId = 'ocid1.tenancy.oc1..aaaaaaaaji4ohhurx2uydbjbvd2skio5ad5tp2nvu4azii2oy5tu5aol4phq'
+const auth = new SimpleAuthentication(process.env);
+const compartmentId = 'ocid1.tenancy.oc1..aaaaaaaaji4ohhurx2uydbjbvd2skio5ad5tp2nvu4azii2oy5tu5aol4phq';
 describe('vault', () => {
-	const vault = new Vault(auth)
+	const vault = new Vault(auth);
 	before(async () => {
-		const health = await auth.connect()
-		assert.ok(health)
-	})
+		const health = await auth.connect();
+		assert.ok(health);
+	});
 	it('list', async () => {
 
 
-		const result = await vault.list(compartmentId)
-		console.debug(result)
-	})
-})
+		const result = await vault.list(compartmentId);
+		console.debug(result);
+	});
+});
 describe('key', function () {
-	this.timeout(0)
-	const vaultId = 'ocid1.vault.oc1.ap-singapore-1.enrhpwtoaabem.abzwsljrk57oclvejakgkh42rblwi7dmymmhnfrmt7nmloagt24mcrpl236q'
+	this.timeout(0);
+	const vaultId = 'ocid1.vault.oc1.ap-singapore-1.enrhpwtoaabem.abzwsljrk57oclvejakgkh42rblwi7dmymmhnfrmt7nmloagt24mcrpl236q';
 
-	const vault = new Vault(auth)
-	let oneVault
+	const vault = new Vault(auth);
+	let oneVault;
 	before(async () => {
-		const health = await auth.connect()
-		assert.ok(health)
-		oneVault = await vault.get(vaultId)
-	})
+		const health = await auth.connect();
+		assert.ok(health);
+		oneVault = await vault.get(vaultId);
+	});
 	it('list', async () => {
 
-		const key = new Key(auth, oneVault)
+		const key = new Key(auth, oneVault);
 
-		const keyList = await key.list()
-		console.debug(keyList)
-	})
+		const keyList = await key.list();
+		console.debug(keyList);
+	});
 	it('ecdsa sign and verify', async () => {
 		// ec key
-		const keyID = 'ocid1.key.oc1.ap-singapore-1.enrhpwtoaabem.abzwsljrizggn5jlznyv7j64ccchehu6wc6vmfllyobilups4ahhp34pzyqq'
-		const key = new Key(auth, oneVault, keyID)
-		const message = 'EXAMPLE-message-Value'
-		const signature = await key.sign(message)
+		const keyID = 'ocid1.key.oc1.ap-singapore-1.enrhpwtoaabem.abzwsljrizggn5jlznyv7j64ccchehu6wc6vmfllyobilups4ahhp34pzyqq';
+		const key = new Key(auth, oneVault, keyID);
+		const message = 'EXAMPLE-message-Value';
+		const signature = await key.sign(message);
 
-		const result = await key.verify(signature, message)
-		assert.ok(result)
-	})
+		const result = await key.verify(signature, message);
+		assert.ok(result);
+	});
+	it('ecdsa sign and verify: digest(sha256):digest cannot be longer than 32 bytes', async () => {
+		// WARN: hex encoding make digest too long, please use default one
+		const sha2_256 = (data) => crypto.createHash('sha256').update(data).digest();
+
+		const keyID = 'ocid1.key.oc1.ap-singapore-1.enrhpwtoaabem.abzwsljrizggn5jlznyv7j64ccchehu6wc6vmfllyobilups4ahhp34pzyqq';
+		const key = new Key(auth, oneVault, keyID);
+		const message = sha2_256('EXAMPLE-message-Value');
+		const signature = await key.sign(message, undefined, undefined, true);
+
+		const result = await key.verify(signature, message, undefined, undefined, true);
+		assert.ok(result);
+	});
 	it('inspect ec', async () => {
 		// ec key
-		const keyID = 'ocid1.key.oc1.ap-singapore-1.enrhpwtoaabem.abzwsljrizggn5jlznyv7j64ccchehu6wc6vmfllyobilups4ahhp34pzyqq'
-		const key = new Key(auth, oneVault, keyID)
-		const keyInfo = await key.get()
-		assert.strictEqual(keyInfo.keyShape.algorithm, 'ECDSA')
+		const keyID = 'ocid1.key.oc1.ap-singapore-1.enrhpwtoaabem.abzwsljrizggn5jlznyv7j64ccchehu6wc6vmfllyobilups4ahhp34pzyqq';
+		const key = new Key(auth, oneVault, keyID);
+		const keyInfo = await key.get();
+		assert.strictEqual(keyInfo.keyShape.algorithm, 'ECDSA');
 
-	})
+	});
 	it('rsa inspect', async () => {
 		// rsa key
-		const keyID = 'ocid1.key.oc1.ap-singapore-1.enrhpwtoaabem.abzwsljrh263ohsu2o7rfg4eel67wcc6akv4575e52atvziwnh2qchrm6icq'
-		const key = new Key(auth, oneVault, keyID)
-		const keyInfo = await key.get()
-		assert.strictEqual(keyInfo.keyShape.algorithm, 'RSA')
-		console.debug(keyInfo)
-	})
+		const keyID = 'ocid1.key.oc1.ap-singapore-1.enrhpwtoaabem.abzwsljrh263ohsu2o7rfg4eel67wcc6akv4575e52atvziwnh2qchrm6icq';
+		const key = new Key(auth, oneVault, keyID);
+		const keyInfo = await key.get();
+		assert.strictEqual(keyInfo.keyShape.algorithm, 'RSA');
+		console.debug(keyInfo);
+	});
 	it('rsa sign and verify', async () => {
 		// rsa key
-		const keyID = 'ocid1.key.oc1.ap-singapore-1.enrhpwtoaabem.abzwsljrh263ohsu2o7rfg4eel67wcc6akv4575e52atvziwnh2qchrm6icq'
-		const key = new Key(auth, oneVault, keyID)
-		const message = 'EXAMPLE-message-Value'
+		const keyID = 'ocid1.key.oc1.ap-singapore-1.enrhpwtoaabem.abzwsljrh263ohsu2o7rfg4eel67wcc6akv4575e52atvziwnh2qchrm6icq';
+		const key = new Key(auth, oneVault, keyID);
+		const message = 'EXAMPLE-message-Value';
 
-		const signature = await key.sign(message)
-		const result = await key.verify(signature, message,)
-		assert.ok(result)
-	})
-})
+		const signature = await key.sign(message);
+		const result = await key.verify(signature, message,);
+		assert.ok(result);
+	});
+});
 
 describe('cli', function () {
-	this.timeout(0)
+	this.timeout(0);
 	it.skip('RSA sign', async () => {
-		const keyID = 'ocid1.key.oc1.ap-singapore-1.enrhpwtoaabem.abzwsljrh263ohsu2o7rfg4eel67wcc6akv4575e52atvziwnh2qchrm6icq'
-		const message = Buffer.from('EXAMPLE-message-Value').toString('base64')
+		const keyID = 'ocid1.key.oc1.ap-singapore-1.enrhpwtoaabem.abzwsljrh263ohsu2o7rfg4eel67wcc6akv4575e52atvziwnh2qchrm6icq';
+		const message = Buffer.from('EXAMPLE-message-Value').toString('base64');
 
-		const pss = 'SHA_256_RSA_PKCS_PSS'
-		const endpoint = 'https://enrhpwtoaabem-crypto.kms.ap-singapore-1.oci.oraclecloud.com'
-		const cmd = `oci kms crypto signed-data sign --key-id ${keyID} --message ${message} --signing-algorithm ${pss} --endpoint ${endpoint}`
-		const {data} = JSON.parse(execSync(cmd))
-		console.info(data)
-	})
-})
+		const pss = 'SHA_256_RSA_PKCS_PSS';
+		const endpoint = 'https://enrhpwtoaabem-crypto.kms.ap-singapore-1.oci.oraclecloud.com';
+		const cmd = `oci kms crypto signed-data sign --key-id ${keyID} --message ${message} --signing-algorithm ${pss} --endpoint ${endpoint}`;
+		const {data} = JSON.parse(execSync(cmd));
+		console.info(data);
+	});
+});
