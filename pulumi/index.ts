d35df76cfa91c08e476d847af50ab02a8c30d087
David Liu <david-khala@hotmail.com>
2023-02-05 23:08:29 +0800

    cannot reuse @davidkhala/pulumi-oci

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,8 +1,8 @@
-import * as pulumi from '@pulumi/pulumi';
+import {CustomResourceOptions} from '@pulumi/pulumi';
 import {CompartmentArgs, Compartment} from '@pulumi/oci/identity/compartment';
 
 export class Tenancy extends Compartment {
-	constructor(name: string, args: CompartmentArgs = {description: name, name}, opts?: pulumi.CustomResourceOptions) {
+	constructor(name: string, args: CompartmentArgs = {description: name, name}, opts?: CustomResourceOptions) {
 		super(name, args, opts);
 	}
 }
